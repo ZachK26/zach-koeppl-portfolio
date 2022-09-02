fa17ecc7afe10890a4f62c95e40cc448fba42ade
ZachK26 <soccerremix9@gmail.com>
2022-09-02 11:14:26 -0500

    added some basic css and projects

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import flask
+from flask import (Flask,
+render_template,
+request,
+session,
+redirect,
+url_for)
+
+app = Flask(__name__)
+app.secret_key = 'keylol'
+
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    return render_template('index.html')
