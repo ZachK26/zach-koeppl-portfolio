4fe16646d1cd9b642af4598f0f5802b1dbe73f68
Zachary Koeppl <63168062+ZachK26@users.noreply.github.com>
2022-11-03 12:08:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ request,
 session,
 redirect,
 url_for)
-from flask_frozen import Freezer
 
 app = Flask(__name__)
 app.secret_key = 'keylol'
 
-#freezer = Freezer(app)
-#
-#if __name__ == '__main__':
-#    freezer.freeze()
-
 @app.route('/', methods = ['GET', 'POST'])
 def login():
     return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
