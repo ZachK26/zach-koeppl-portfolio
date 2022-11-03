a4c16ab0081ddc7651b27b7906b46cf9bac085d4
Zachary Koeppl <63168062+ZachK26@users.noreply.github.com>
2022-11-03 11:46:21 -0500

    Resized some more

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ request,
 session,
 redirect,
 url_for)
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 app.secret_key = 'keylol'
+
+#freezer = Freezer(app)
+#
+#if __name__ == '__main__':
+#    freezer.freeze()
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
